f60b0c633d7694591880ffdb909ea77ed951fd94
Santhoshvellingiri <santhoshvellingiri100@gmail.com>
2026-05-25 11:25:58 +0530

    Update contact page image URL

--- src/meta/data.js
+++ src/meta/data.js
@@ -207,5 +207,5 @@ about_page02_carosuls: [
       tooltip: "Instagram",
     },
   ],
-  contact_page_image:"https://images.unsplash.com/photo-1587560699334-bea93391dcef?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29udGFjdCUyMHVzfGVufDB8fDB8fHww"
+  contact_page_image:"https://i.pinimg.com/webp85/736x/6b/8f/6e/6b8f6e41f2104eec043a28b7293b012a.webp"
 }
